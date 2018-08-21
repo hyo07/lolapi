b9e983ac29d4880d2686f7a8c067f13f977fae66
hyo07 <yutaka727kato@gmail.com>
2018-08-21 18:58:06 +0900

    CSVで保存

--- lol.py
+++ lol.py
@@ -1,18 +1,16 @@
 from riotwatcher import RiotWatcher
 from time import sleep
 import numpy as np
+import pandas as pd
 
-watcher = RiotWatcher("RGAPI-4cc4a590-7420-45fa-8f9e-4e0a7e6da108")
+watcher = RiotWatcher("{APIkey}")
 
 region = "jp1"
-name = "kinopy"
+name = "{サモナーネーム}"
 
 summoner = watcher.summoner.by_name(region, name)
-sleep(5)
 ranked_state = watcher.league.positions_by_summoner(region, summoner["id"])
-sleep(5)
 recentmatchlists = watcher.match.matchlist_by_account(region,summoner["accountId"])
-sleep(10)
 
 matches = recentmatchlists['matches']
 match_detail = []
@@ -52,12 +50,13 @@ for x in range(20):
                 data_list.append([match_detail[x]["participants"][player_num]["stats"].get(i,0) for i in decision_list])
                 result_lsit.append(match_detail[x]["participants"][player_num]["stats"]["win"])
 
+df1 = pd.DataFrame(data_list, columns=decision_list)
+df2 = pd.DataFrame(result_lsit)
 
-data = np.array(data_list)
-result = np.array(result_lsit)
-
-print(summoner)
+df1.to_csv("dataset.csv",sep=",", mode='a',header=False)
+df2.to_csv("rabel.csv",sep=",", mode='a',header=False)
+#print(summoner)
+#print("")
+print(df1)
 print("")
-print(data)
-print("")
-print(result)
+print(df2)
